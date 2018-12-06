c4cd7f7fc11647755c28bfffdfa2e133ec7f6d9f
dwkns <dwkns@me.com>
2018-12-06 22:42:17 +0000

    Improved bundle.js detection

--- source/js/bundle.js
+++
@@ -1,6 +0,0 @@
-// source/js/main.js
-
-import { helloWorld } from './helloWorld.js'; 
-
-const outputString = `Javascript Modules... <span class="testClass">${helloWorld}</span>`;
-document.getElementById('outputField').innerHTML = outputString;
