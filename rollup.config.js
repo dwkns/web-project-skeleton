1f5fde58c78a50a5999ff2fbcfcbc7c23b85cfa4
dwkns <dwkns@me.com>
2018-12-06 22:51:14 +0000

    Fixed name not chaning bug.

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
-    input: 'source/tmp/bundle.js',
+    input: 'source/js/bundle.js',
     output: {
         file: 'build/js/bundle.js',
         format: 'iife'
